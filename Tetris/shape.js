68c40dde7b2d79fae6a1d90efe930a2a8a1c6e27
Kristian-A <kristian.zh.apostolov@gmail.com>
2018-03-16 19:24:58 +0200

    tetris

--- Tetris/shape.js
+++ Tetris/shape.js
@@ -1,4 +1,4 @@
-let blockSize = 32;
+let blockSize =32;
 
 function multArr(y, x) {
 	let arr = [];
@@ -18,14 +18,16 @@ class Block {
 		this.y = yOff;
 	}
 
-	move(x, y) {
-		let lastX = this.x;
-		let lastY = this.y;
-		gameGrid[lastY][lastX] = false;
-		this.x = (x + this.xOff);
-		this.y = (y + this.yOff);
-		gameGrid[y + this.yOff][x + this.xOff] = true;
-		
+	move(x, y, param = false) {
+		gameGrid[this.y][this.x] = null;
+		if (param) {
+			this.x = x;
+			this.y = y;	
+			gameGrid[y][x] = this;
+		} else {
+			this.x = (x + this.xOff);
+			this.y = (y + this.yOff);	
+		}
 	}
 
 	draw() {
@@ -40,64 +42,104 @@ class Figure {
 
 	constructor(arr) {
 		this.blocks = [];
-		this.furthestX = 0;
+		this.rightX = 0;
+		this.leftX = 4;
 		for (let y = 0; y < 2; y++) {
 			for (let x = 0; x < 4; x++) {
 				if (arr[y][x]) {
 					this.blocks.push(new Block(x, y));
-					if (this.furthestX < x) {
-						this.furthestX = x;
+					if (this.rightX < x) {
+						this.rightX = x;
+					}
+					if (this.leftX > x) {
+						this.leftX = x;
 					}
 				}
 			}
 		}	
 		this.x = 5;
 		this.y = 0;
-		this.move(5, 0);
 	}
 
 	right() {
-		if (this.x < 9 - this.furthestX) {
+		if (!this.blockRight()) {
 			this.move(this.x+1, this.y);
 		}
 	}
 
 	left() {
-		if (this.x > 0) {
-		this.move(this.x-1, this.y);
+		if (!this.blockLeft()) {
+			this.move(this.x-1, this.y);
 		}
 	}
 
 	down() {
-		if (this.y < 19) {
+		if (!this.blockDown()) {
 			this.move(this.x, this.y+1);
 		}
+	}
+
+	blockRight() {
+		for (let i = 0; i < this.blocks.length; i++) {
+			let current = this.blocks[i];
+			if (this.x == 9 - this.rightX || gameGrid[current.y][current.x+1] != null) {
+				if (!this.blockInFigure(current.x+1, current.y)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	blockLeft() {
+		for (let i = 0; i < this.blocks.length; i++) {
+			let current = this.blocks[i];
+			if (this.x == this.leftX || gameGrid[current.y][current.x-1] != null) {
+				if (!this.blockInFigure(current.x-1, current.y)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	blockDown() {
+		for (let i = 0; i < this.blocks.length; i++) {
+			let current = this.blocks[i];
+			if (current.y + 1 == 20 || gameGrid[current.y + 1][current.x] != null) {
+				if (!this.blockInFigure(current.x, current.y+1)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	blockInFigure(x, y) {
+		for (var i = 0; i < this.blocks.length; i++) {	
+			if (this.blocks[i].x == x && this.blocks[i].y == y) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	move(x, y) {
 		this.blocks.forEach(block => {
 			block.move(x, y);
 		});
+		this.blocks.forEach(block => {
+			gameGrid[block.y][block.x] = block;
+		});
 		this.x = x;
 		this.y = y;
+
 	}
 
 	draw() { 
 		this.blocks.forEach(block => {
 			block.draw();
 		});
-	}
-
-	blockBelow() {
-		for (let i = 0; i < this.blocks.length; i++) {
-			let current = this.blocks[i];
-			if (current.y + 1 == 20 || gameGrid[current.y + 1][current.x]) {
-				//if (!gameGrid[current.y+1][current.x] in this.blocks) {
-					return true;
-				//}
-			}
-		}
-		return false;
 	}
 
 }
--- Tetris/sketch.js
+++ Tetris/sketch.js
@@ -1,24 +1,57 @@
-let gameGrid;
+let gameGrid = [];
 let currentFigure;
 let tickTimer;
 let figures = [];
-let visibleFigures = [];
 let figureIndex = 1;
+let score = 0;
 
 function setup() {
 	createCanvas(320, 640);
 	gameGrid = multArr(20, 10);
 	gameGrid.forEach((layer) => {
-		layer.fill(false);
+		layer.fill(null);
 	});
 
 	figures = [];
-	for (let i = 0; i < 5; i++) { 
-		figures.push(new Figure([[1, 1, 1, 1], [0, 0, 0, 0]]));
+	figures.push(new Figure([[1, 1, 1, 1], [0, 0, 0, 0]]));
+	figures.push(new Figure([[1, 1, 1, 0], [0, 0, 0, 0]]));
+	figures.push(new Figure([[1, 1, 1, 0], [0, 0, 0, 0]]));
+	for (let i = 0; i < 2; i++) { 
+		figures.push(new Figure([[0, 0, 1, 0], [1, 1, 1, 0]]));
 	}
 	currentFigure = figures[0];
-	visibleFigures.push(figures[0]);
 	tickTimer = new Date();
+}
+
+function shiftDown(lowest) {
+	for (let i = lowest; i >= 0; i--) {
+		gameGrid[i].forEach(block => {
+			if (block != null) {
+				block.move(block.x, block.y+1, true);
+			}
+		}); 
+	}
+	score+=100;
+	console.log(score); 
+}
+
+function checkLayers() {
+	for (let i = 0; i < gameGrid.length; i++) {
+		let layer = gameGrid[i];
+		let filled = true;
+		for (let j = 0; j < layer.length; j++) {
+			if (layer[j] == null) {
+				filled = false;
+			}	
+		}
+		if (filled) {
+
+			for (let j = 0; j < layer.length; j++) {
+				layer[j] = null;
+			}
+			shiftDown(i);
+		}
+	}
 }
 
 function draw() {
@@ -27,17 +60,20 @@ function draw() {
 	let currentTime = new Date().getTime()
 	if (currentTime - tickTimer.getTime() > 400) {
 		tickTimer.setTime(currentTime);
-		if (currentFigure.blockBelow()) {
+		if (currentFigure.blockDown()) {
 			currentFigure = figures[figureIndex];
-			visibleFigures.push(currentFigure);
 			figureIndex++;
 		}
 		currentFigure.down()
 	}
-
-	visibleFigures.forEach(figure => {
-		figure.draw();
+	gameGrid.forEach(layer => {
+		layer.forEach(block => { 
+			if (block != null) {
+				block.draw();
+			}
+		})
 	});
+	checkLayers();
 }
 
 function keyTyped() {
